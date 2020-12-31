fcf40d96b746af0c99c760d750064892e9599f00
Zain Fareed <zainfareed093@gmail.com>
2020-12-31 01:57:22 -0800

    Created POJO Classes

---
+++ app/src/main/java/com/example/assignment_1/WeatherData/Daily.java
@@ -0,0 +1,147 @@
+
+package com.example.assignment_1.WeatherData;
+
+import com.example.assignment_1.FeelsLike;
+import com.example.assignment_1.Weather;
+
+import java.util.List;
+
+public class Daily {
+
+    private Integer dt;
+    private Integer sunrise;
+    private Integer sunset;
+    private Temp temp;
+    private FeelsLike feelsLike;
+    private Integer pressure;
+    private Integer humidity;
+    private Double dewPoint;
+    private Double windSpeed;
+    private Integer windDeg;
+    private List<Weather> weather = null;
+    private Integer clouds;
+    private Integer pop;
+    private Integer uvi;
+    private Double rain;
+
+    public Integer getDt() {
+        return dt;
+    }
+
+    public void setDt(Integer dt) {
+        this.dt = dt;
+    }
+
+    public Integer getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(Integer sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public Integer getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(Integer sunset) {
+        this.sunset = sunset;
+    }
+
+    public Temp getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Temp temp) {
+        this.temp = temp;
+    }
+
+    public FeelsLike getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(FeelsLike feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public Integer getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(Integer pressure) {
+        this.pressure = pressure;
+    }
+
+    public Integer getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Integer humidity) {
+        this.humidity = humidity;
+    }
+
+    public Double getDewPoint() {
+        return dewPoint;
+    }
+
+    public void setDewPoint(Double dewPoint) {
+        this.dewPoint = dewPoint;
+    }
+
+    public Double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public Integer getWindDeg() {
+        return windDeg;
+    }
+
+    public void setWindDeg(Integer windDeg) {
+        this.windDeg = windDeg;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public Integer getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(Integer clouds) {
+        this.clouds = clouds;
+    }
+
+    public Integer getPop() {
+        return pop;
+    }
+
+    public void setPop(Integer pop) {
+        this.pop = pop;
+    }
+
+    public Integer getUvi() {
+        return uvi;
+    }
+
+    public void setUvi(Integer uvi) {
+        this.uvi = uvi;
+    }
+
+    public Double getRain() {
+        return rain;
+    }
+
+    public void setRain(Double rain) {
+        this.rain = rain;
+    }
+
+}
---
+++ app/src/main/java/com/example/assignment_1/WeatherData/WeatherData.java
@@ -0,0 +1,54 @@
+
+package com.example.assignment_1.WeatherData;
+
+import java.util.List;
+
+public class WeatherData {
+
+    private Double lat;
+    private Double lon;
+    private String timezone;
+    private Integer timezoneOffset;
+    private List<Daily> daily = null;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public Integer getTimezoneOffset() {
+        return timezoneOffset;
+    }
+
+    public void setTimezoneOffset(Integer timezoneOffset) {
+        this.timezoneOffset = timezoneOffset;
+    }
+
+    public List<Daily> getDaily() {
+        return daily;
+    }
+
+    public void setDaily(List<Daily> daily) {
+        this.daily = daily;
+    }
+
+}
